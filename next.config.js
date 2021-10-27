7f6cdd2d63d724d00b902bf42852a91d14be4faf
utiyama <shou0604pro@icloud.com>
2021-10-27 20:49:43 +0900

    設定の変更

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+	i18n: { locales: ["ja"], defaultLocale: "ja" },
 	reactStrictMode: true,
+	poweredByHeader: false,
 	images: {
 		formats: ["image/avif", "image/webp"],
 	},
