898dd7c28a6428c0bf763a3c1dbf20b8c31ebebe
utiyama <shou0604pro@icloud.com>
2021-10-27 16:13:49 +0900

    Next.js のバージョンアップと eslint, tsconfigの改善

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-	return <div className="text-red-500 text-center">Hello World</div>;
+	return <div className="text-3xl text-center text-red-500">Hello World</div>;
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./src/**/*.{ts,tsx}"],
-  darkMode: false,
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	mode: "jit",
+	purge: ["./src/**/*.{ts,tsx}"],
+	darkMode: false,
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
