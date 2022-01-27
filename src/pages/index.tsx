5858667eddec53253b49ab9bc8f64b3d408b743a
utiyama <shou0604pro@icloud.com>
2022-01-28 00:51:55 +0900

    パッケージのアップグレード

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div>
-      <p className="text-3xl text-center text-red-500">Hello World</p>
+      <p className="text-center text-3xl text-red-500">Hello World</p>
     </div>
   );
 };
