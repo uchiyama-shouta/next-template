ea70cba0527f336401cc2b2fb8c416cfd728a6d3
utiyama <ninjin0604@gmail.com>
2022-05-25 23:38:05 +0900

    機能拡張

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/order */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  pwa: { dest: "public", runtimeCaching },
+});
+
+const withPlugins = require("next-compose-plugins");
+
+/** @type {import('next').NextConfig} */
+const config = {
+  swcMinify: true,
+  i18n: { locales: ["ja"], defaultLocale: "ja" },
+  reactStrictMode: true,
+  poweredByHeader: false,
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
+};
+
+module.exports = withPlugins([], {
+  ...config,
+});
